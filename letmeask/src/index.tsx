e092bec47630cda3b8fc0b113737d18b1d67f366
Victor-Aguiar-Ribeiro <victor_a.ribeiro@outlook.com>
2021-11-04 13:35:06 -0300

    Starting React project with TypesCript
    Created project --> `yarn create react-app letmeask --template typescript`
    Cleaned folder structure to start just with the necessary
    Added /images folder to ./public directory
    Added /components folder to ./src directory

---
+++ letmeask/src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import { Button } from './components/Button';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <Button />
+    <Button />
+    <Button />
+    <Button />
+    <Button />
+    <Button />
+    <Button />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
