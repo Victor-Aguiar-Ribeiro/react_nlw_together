cf977f4c80baacaf244d16fdbab0eab728bc1ebf
Victor-Aguiar-Ribeiro <victor_a.ribeiro@outlook.com>
2021-11-05 11:24:41 -0300

    Added .env keys for firebase

--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNbuzUuu3JvCdcTSP6i-WOsKo5yuCxCr8",
-  authDomain: "nlw-letmeask-c6ff6.firebaseapp.com",
-  databaseURL: "https://nlw-letmeask-c6ff6-default-rtdb.firebaseio.com",
-  projectId: "nlw-letmeask-c6ff6",
-  storageBucket: "nlw-letmeask-c6ff6.appspot.com",
-  messagingSenderId: "946667630136",
-  appId: "1:946667630136:web:a26c43f8a138f12f2f40cd"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
